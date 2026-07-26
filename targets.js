766ea6f5f337deb69ad968fe643f6c2fdb1f52d0
phaik-byte <niko.huhtaniemi@gmail.com>
2026-07-26 11:13:26 +0300

    Create TARGET_DOMAINS list in targets.js
    Added a list of target domains categorized by industry.

---
+++ targets.js
@@ -0,0 +1,78 @@
+// ============================================
+// KOHDELISTA - Muokkaa tätä vapaasti!
+// ============================================
+// Lisää tai poista domainit tähän listaan.
+// Jokainen domain erikseen lainausmerkeissä ja pilkulla erotettuna.
+// Viimeisen domainin perään ei tule pilkkua.
+// ============================================
+
+const TARGET_DOMAINS = [
+    // Ravintolat ja kahvilat
+    'ravintola.fi', 
+    'kahvila.fi', 
+    'ravintolakoti.fi', 
+    'lounasravintola.fi',
+    'konditoria.fi', 
+    'leipomo.fi', 
+    'pizzeria.fi', 
+    'grilli.fi',
+    
+    // Kauneus ja hyvinvointi
+    'parturi.fi', 
+    'kampaamo.fi', 
+    'hieroja.fi', 
+    'fysioterapia.fi',
+    'kosmetologi.fi', 
+    'kuntosali.fi',
+    
+    // Rakentaminen ja remontti
+    'rakennus.fi', 
+    'remontti.fi', 
+    'maalarit.fi', 
+    'lvi.fi',
+    'sahkotyo.fi', 
+    'lattianhoito.fi',
+    
+    // Kiinteistöt ja asuminen
+    'kiinteisto.fi', 
+    'asunto.fi', 
+    'vuokraus.fi', 
+    'sisustus.fi',
+    
+    // Kuljetus ja logistiikka
+    'kuljetus.fi', 
+    'logistiikka.fi', 
+    'muutto.fi', 
+    'taksi.fi',
+    
+    // Tietotekniikka ja digi
+    'it-palvelut.fi', 
+    'verkkosivut.fi', 
+    'digimarkkinointi.fi',
+    'ohjelmointi.fi', 
+    'tietoturva.fi',
+    
+    // Koulutus ja konsultointi
+    'koulutus.fi', 
+    'konsultointi.fi', 
+    'valmennus.fi', 
+    'kielikoulu.fi',
+    
+    // Terveys ja hyvinvointi
+    'optikko.fi', 
+    'apteekki.fi', 
+    'ravitsemus.fi',
+    
+    // Kauppa ja verkkokauppa
+    'verkkokauppa.fi', 
+    'puutarha.fi', 
+    'kukkakauppa.fi',
+    
+    // Auto ja liikenne
+    'autokorjaamo.fi', 
+    'renkaat.fi', 
+    'varaosat.fi'
+];
+
+// Älä muokkaa tätä riviä - se vie listan eteenpäin
+module.exports = { TARGET_DOMAINS };
